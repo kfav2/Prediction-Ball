0d865859d1acc1087a442eb9f35183b34044a313
KFAV <kofava14@gmail.com>
2021-12-27 15:21:43 -0800

    Prediction Ball - Step-by-Step Guide to Build a "Magic 8 Ball"-style iOS App for Absolute Beginners

--- Prediction Ball/ViewController.swift
+++ Prediction Ball/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    var messages = ["It is certain.",
+                   "It is decidedly so.",
+                    "Without a doubt.",
+                    "Yes definitely.",
+                    "You may rely on it.",
+                    "As I see it, yes.",
+                    "Reply hazy, try again.",
+                    "Ask again later.",
+                    "Better not tell you now.",
+                    "Don't count on it.",
+                    "My reply is no.",
+                    "My sources say no."]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = ""
     }
 
-
+    @IBAction func questionButtonPressed(_ sender: UIButton) {
+        let randomMessage = messages.randomElement()
+        messageLabel.text = randomMessage
+    }
+    
 }
 
